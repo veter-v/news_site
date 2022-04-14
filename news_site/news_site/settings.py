1f90b592e0c6c4be1d13dd58542e9be180b15703
veter <vikylya1709@gmail.com>
2022-04-15 00:42:55 +0500

    Create News Model

--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'  # путь до папки с фото
+MEDIA_URL = 'media/'
