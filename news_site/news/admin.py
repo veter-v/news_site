ab8e2a28fef263519ab5194cbaf0148f8204b0cb
veter <vikylya1709@gmail.com>
2022-04-16 21:12:57 +0500

    Updated index.html with bootstrap

--- news_site/news/admin.py
+++ news_site/news/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import News, Category
 
 # Register your models here.
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content', 'category')
+    list_editable = ('is_published', )
+    list_filter = ('is_published', 'category')
+
+
+admin.site.register(News, NewsAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+
--- news_site/news/models.py
+++ news_site/news/models.py
@@ -3,12 +3,34 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='Name of the news', db_index=True)
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)  # добавятся дата и время и редактироваться больше не будут
-    updated_at = models.DateTimeField(auto_now=True)  # будут редактироваться при каждом редактирование записи
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')  # сохр. путь до img, указ. путь разбивается на папки по д.
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Publication date')  # добавятся дата и время и ред. больше не будут
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name='Update date')  # будут редакт. при каждом редактирование записи
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',
+                              blank=True)  # сохр. путь до img, указ. путь разбивается на папки по д.
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True)
+    # название модели передаём как строку, если она определена позже
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'  # название новости в ед.ч. в админке
+        verbose_name_plural = 'List of news'  # Название в мн.ч в админке
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='News category')
+
+    class Meta:
+        verbose_name = 'News category'
+        verbose_name_plural = 'News categories'
+        ordering = ['title']
 
     def __str__(self):
         return self.title
